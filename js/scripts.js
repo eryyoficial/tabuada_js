53fd796177ba0735f4bec60cfa6af1ae6628366e
Hermínio Da Silva André Francisco <154982502+eryyoficial@users.noreply.github.com>
2024-11-23 14:27:33 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,44 @@
-// Seleção de elementos
-const multiplicationForm = document.querySelector("#multiplication-form");
-const numberInput = document.querySelector("#number");
-const multiplicationInput = document.querySelector("#multiplicator");
+//========================================== DECLARAÇÃO DOS SELECTORES ==========================================
+const formulario = document.querySelector("#multiplication-form");
+const numberOne = document.querySelector("#number");
+const numberTwo = document.querySelector("#multiplicator");
+const titleNumber = document.querySelector("#multiplication-title span");
+const tabela = document.querySelector("#multiplication-operations");
 
-const multiplicationTitle = document.querySelector(
-  "#multiplication-title span"
-);
-const multiplicationTable = document.querySelector(
-  "#multiplication-operations"
-);
+//========================================== FUNÇÃO PARA CRIAR A TABUADA ==========================================
+function createTable(one, two) {
+  tabela.innerHTML = "";
+  titleNumber.innerHTML = one;
+  for (let index = 1; index <= two; index++) {
+    const resusltado = one * index;
 
-// Funções
-const createTable = (number, multiplicatorNumber) => {
-  multiplicationTable.innerHTML = "";
-
-  for (i = 1; i <= multiplicatorNumber; i++) {
-    const result = number * i;
-
-    const template = `<div class="row">
-            <div class="operation">${number} x ${i} = </div>
-            <div class="result">${result}</div>
+    //Criando um template HTML a partir do JS
+    const template = `
+        <div class="row">
+          <div class="operation">${one} x ${index} = </div>
+          <div class="resu">${resusltado}</div>
         </div>`;
 
-    const parser = new DOMParser();
-    const htmlTemplate = parser.parseFromString(template, "text/html");
-    const row = htmlTemplate.querySelector(".row");
+    //Convertendo o template
+    const parsa = new DOMParser();
+    const htmlConv = parsa.parseFromString(template, "text/html");
+    const row = htmlConv.querySelector(".row");
 
-    multiplicationTable.appendChild(row);
+    //Adicionando cada Linha de operação na Tabuada
+    const finalLine = tabela.appendChild(row);
   }
 
-  multiplicationTitle.innerText = number;
-};
+  console.log("Tabuada feita com Sucesso!");
+}
 
-// Eventos
-multiplicationForm.addEventListener("submit", (e) => {
+//========================================== EVENTO PARA SUBMETER A TABUADA ==========================================
+formulario.addEventListener("submit", (e) => {
   e.preventDefault();
+  const newNumberOne = +numberOne.value;
+  const newNumberTwo = +numberTwo.value;
 
-  const multiplicationNumber = numberInput.value;
-
-  const multiplicatorNumber = +multiplicationInput.value;
-
-  if (!multiplicationNumber || !multiplicatorNumber) return;
-
-  createTable(multiplicationNumber, multiplicatorNumber);
+  if (!newNumberOne || !newNumberTwo) {
+    return alert("Preencha todos os campos");
+  }
+  createTable(newNumberOne, newNumberTwo);
 });
